099ca9a6f873d3009e1791cbd0d7406e0ba75989
michalklawik <WejhiT261f>
2018-07-07 19:07:09 +0200

    Added Open Dialog

--- src/main/java/kata/App.java
+++ src/main/java/kata/App.java
@@ -1,9 +1,26 @@
 package kata;
 
+import javax.swing.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class App
 {
     public static void main( String[] args )
     {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new File("."));
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        chooser.setAcceptAllFileFilterUsed(false);
+        List<File> files = new ArrayList<>();
 
+        if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            files = Arrays.asList(chooser.getCurrentDirectory());
+            System.out.println("Folder: " + chooser.getCurrentDirectory());
+        } else {
+            JOptionPane.showMessageDialog(null, "Nie wybrano folderu.", "Błąd", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
