2a0c0a733f090c12210d8539a3a3bbaab3298b50
michalklawik <WejhiT261f>
2018-07-07 20:09:39 +0200

    Added zip functionality

--- src/main/java/kata/App.java
+++ src/main/java/kata/App.java
@@ -1,15 +1,20 @@
 package kata;
 
 import javax.swing.*;
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException {
+        String path = getDirectoryPath();
+        zipDirectory(path);
+    }
+
+    private static String getDirectoryPath() {
         JFileChooser chooser = new JFileChooser();
         chooser.setCurrentDirectory(new File("."));
         chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
@@ -17,10 +22,40 @@ public class App
         List<File> files = new ArrayList<>();
 
         if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-            files = Arrays.asList(chooser.getCurrentDirectory());
-            System.out.println("Folder: " + chooser.getCurrentDirectory());
+            return chooser.getSelectedFile().getAbsolutePath();
         } else {
             JOptionPane.showMessageDialog(null, "Nie wybrano folderu.", "Błąd", JOptionPane.ERROR_MESSAGE);
         }
+        return null;
+    }
+
+    private static void zipDirectory(String path) throws IOException {
+
+        FileOutputStream fos = new FileOutputStream("zipedDire.zip");
+        ZipOutputStream zipOutputStream = new ZipOutputStream(fos);
+        File directoryToZip = new File(path);
+
+        zipFile(directoryToZip, directoryToZip.getName(), zipOutputStream);
+        zipOutputStream.close();
+        fos.close();
+    }
+
+    private static void zipFile(File directoryToZip, String name, ZipOutputStream zipOutputStream) throws IOException {
+        if (directoryToZip.isDirectory()) {
+            File[] files = directoryToZip.listFiles();
+            for (File file : files) {
+                zipFile(file, name + "/" + file.getName(), zipOutputStream);
+            }
+            return;
+        }
+        FileInputStream fis = new FileInputStream(directoryToZip);
+        ZipEntry zipEntry = new ZipEntry(name);
+        zipOutputStream.putNextEntry(zipEntry);
+        byte[] bytes = new byte[1024];
+        int length;
+        while ((length = fis.read(bytes)) >= 0) {
+            zipOutputStream.write(bytes, 0, length);
+        }
+        fis.close();
     }
 }
